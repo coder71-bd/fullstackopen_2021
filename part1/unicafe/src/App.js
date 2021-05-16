791bc85ab3723d52445791824e27bdc505f67450
coder71 <kamrunnaher877@gmail.com>
2021-05-16 10:17:09 +0600

    part1 exercise 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,35 +2,41 @@ import React, { useState } from 'react';
 
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
-const DisplayTotal = ({text, totalPoints}) => <div>{text} {totalPoints}</div>
+const StatisticLine = ({text, value}) => <div>{text} {value}</div>
+
+const Statistics = (props) => {
+  let totalClick = props.good + props.neutral + props.bad
+  let average = props.totalFeedback / totalClick || 0
+  let positivePercantage = (props.good / totalClick) * 100 || 0
+  if(totalClick === 0) return <div>No feedback given</div>
+  return (
+    <>
+      <StatisticLine text='good' value={props.good} />
+      <StatisticLine text='neutral' value={props.neutral} />
+      <StatisticLine text='bad' value={props.bad} />
+      <StatisticLine text='all' value={totalClick} />
+      <StatisticLine text='average' value={average} />
+      <StatisticLine text='positive' value={`${positivePercantage} %`} />
+    </>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const [positive, setPositive] = useState(0)
-  const [zero, setZero] = useState(0)
-  const [negative, setNegative] = useState(0)
-
+  const [totalFeedback, setTotalFeedback] = useState(0)
+ 
   const goodClick = () => {
     setGood(good + 1)
-    setPositive(positive + 1)
-  }
-  const neutralClick = () => {
-    setNeutral(neutral + 1)
-    setZero(0)
+    setTotalFeedback(totalFeedback + 1)
   } 
+  const neutralClick = () => setNeutral(neutral + 1)
   const badClick = () => {
     setBad(bad + 1)
-    setNegative(negative - 1)
+    setTotalFeedback(totalFeedback - 1)
   }
 
-  let totalFeedback = positive + zero + negative
-  let totalClick = good + neutral + bad
-  let average = totalFeedback / totalClick || 0
-  let positivePercantage = (good / totalClick) * 100 || 0
-  
   return (
     <div>
       <h1>give feedback</h1>
@@ -38,12 +44,7 @@ const App = () => {
       <Button handleClick={neutralClick} text='neutral' />
       <Button handleClick={badClick} text='bad' />
       <h1>statistics</h1>
-      <DisplayTotal text='good' totalPoints={good} />
-      <DisplayTotal text='neutral' totalPoints={neutral} />
-      <DisplayTotal text='bad' totalPoints={bad} />
-      <DisplayTotal text='all' totalPoints={totalClick} />
-      <DisplayTotal text='average' totalPoints={average} />
-      <DisplayTotal text='positive' totalPoints={`${positivePercantage} %`} />
+      <Statistics good={good} neutral={neutral} bad={bad} totalFeedback={totalFeedback} />
     </div>
   )
 }
