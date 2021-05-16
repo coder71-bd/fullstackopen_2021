17b1541aa6146e33756ef2554b42c7a107b53a07
coder71 <kamrunnaher877@gmail.com>
2021-05-16 13:06:51 +0600

    part1 exercise 1.13

--- part1/anecdote/src/App.js
+++ part1/anecdote/src/App.js
@@ -11,9 +11,18 @@ function App() {
   ]
   const [selected, setSelected] = useState(0)
   const changeIndex = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+  const [updateVotes, setUpdateVotes] = useState({
+    0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0
+  })
+  const changeVotes = () => {
+    updateVotes[selected] += 1
+    setUpdateVotes({...updateVotes})
+  }
   return (
     <div>
       <div>{anecdotes[selected]}</div>
+      <div>has {updateVotes[selected]} votes</div>
+      <button onClick={changeVotes}>vote</button>
       <button onClick={changeIndex}>next anecdote</button>
     </div>
   );
