ca60cba9e7353d7a4d2899c5846cfed95897c7c1
coder71 <kamrunnaher877@gmail.com>
2021-06-06 20:52:55 +0600

    exercise 2.14 finished

--- part2/countrydata/src/App.js
+++ part2/countrydata/src/App.js
@@ -4,12 +4,15 @@ const App = () => {
   const [countries, setcountries] = useState([])
   const [filter, setFilter] = useState('')
   const [view, setview] = useState(countries)
+  const [weather, setWeather] = useState([])
+
   useEffect(() => {
-    axios.get('https://restcountries.eu/rest/v2/all').then((promise) => {
-      setcountries(promise.data)
-      setview(promise.data)
+    axios.get('https://restcountries.eu/rest/v2/all').then((response) => {
+      setcountries(response.data)
+      setview(response.data)
     })
   }, [])
+
   const handleFilter = (e) => {
     setFilter(e.target.value)
     setview(countries)
@@ -30,6 +33,26 @@ const App = () => {
       return []
     }
   }
+  let capitalCity = 'dhaka'
+  if (filteredCountries().length === 1) {
+    capitalCity = filteredCountries()
+      .map((country) => country.capital)
+      .join()
+  }
+
+  useEffect(() => {
+    const api_key = process.env.REACT_APP_API_KEY
+    const params = {
+      appid: api_key,
+      q: capitalCity,
+    }
+
+      .get('http://api.openweathermap.org/data/2.5/weather', { params })
+      .then((response) => {
+        setWeather(response.data)
+      })
+  }, [capitalCity])
+
   const handleButton = (e) => {
     let viewFilter = filteredCountries().filter(
       (country) => country.name === e.target.id
@@ -50,26 +73,46 @@ const App = () => {
       <div>
         {filteredCountries().length <= 10
           ? filteredCountries().length === 1
-            ? filteredCountries().map((country) => (
-                <div key={country.population}>
-                  <h1>{country.name}</h1>
-                  <div>
-                    <div>capital {country.capital}</div>
-                    <div>population {country.population}</div>
+            ? filteredCountries().map((country) => {
+                return (
+                  <div key={country.population}>
+                    <h1>{country.name}</h1>
+                    <div>
+                      <div>capital {country.capital}</div>
+                      <div>population {country.population}</div>
+                    </div>
+                    <h2>Spoken languages</h2>
+                    <ul>
+                      {country.languages.map((lang) => (
+                        <li key={lang.name}>{lang.name}</li>
+                      ))}
+                    </ul>
+                    <img
+                      src={country.flag}
+                      alt={`flag of ${country.name}`}
+                      width="150px"
+                    />
+                    <h2>Weather in {country.capital}</h2>
+                    <div>
+                      <p>
+                        <strong>temperature:</strong> {weather.main.temp}{' '}
+                        Celsius
+                      </p>
+                      {weather.weather.map((image) => (
+                        <img
+                          key={image.id}
+                          src={`http://openweathermap.org/img/wn/${image.icon}@2x.png`}
+                          alt={image.description}
+                        />
+                      ))}
+                      <p>
+                        <strong>wind:</strong> {weather.wind.speed} mph
+                        direction {weather.wind.deg} deg
+                      </p>
+                    </div>
                   </div>
-                  <h2>languages</h2>
-                  <ul>
-                    {country.languages.map((lang) => (
-                      <li key={lang.name}>{lang.name}</li>
-                    ))}
-                  </ul>
-                  <img
-                    src={country.flag}
-                    alt={`flag of ${country.name}`}
-                    width="150px"
-                  />
-                </div>
-              ))
+                )
+              })
             : filteredCountries().map((country) => {
                 return (
                   <div key={country.name}>
