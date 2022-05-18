3d8f90a085e4e17558cdc08e78141e003e4f8966
coder71 <kamrunnaher877@gmail.com>
2022-05-19 00:51:32 +0600

    part 4 exercise 4.2

---
+++ part4/blogListApp/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+
+mongoose
+  .connect(config.MONGO_URL)
+  .then(() => {
+    logger.info('connected to mongodb');
+  })
+  .catch((error) => {
+    logger.error('Error connecting to mongodb', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use(blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
